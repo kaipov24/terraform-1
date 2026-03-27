cb2137010ccb655697df41a902131ce543b326c1
kaipov24 <kaipovich24@gmail.com>
2026-03-27 22:22:23 +0600

    Task 5

--- task-5/network_security.tf
+++ task-5/network_security.tf
@@ -1,22 +1,22 @@
 resource "aws_security_group" "allow_ssh" {
-  name   = "${var.project_id}-ssh-sg"
+  name        = "${var.project_id}-ssh-sg"
   description = "Allow SSH inbound traffic"
   vpc_id      = var.vpc_id
 
   ingress {
-    description      = "SSH"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = var.allowed_ip_range
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = var.allowed_ip_range
   }
 
   ingress {
-    description      = "ICMP"
-    from_port        = -1
-    to_port          = -1
-    protocol         = "icmp"
-    cidr_blocks      = var.allowed_ip_range
+    description = "ICMP"
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = var.allowed_ip_range
   }
 
   egress {
@@ -27,7 +27,7 @@ resource "aws_security_group" "allow_ssh" {
   }
 
   tags = {
-    Name = "${var.project_id}-ssh-sg"
+    Name    = "${var.project_id}-ssh-sg"
     Project = var.project_id
   }
 }
@@ -66,24 +66,24 @@ resource "aws_security_group" "public_http" {
 }
 
 resource "aws_security_group" "private_http" {
-  name   = "${var.project_id}-private-http-sg"
+  name        = "${var.project_id}-private-http-sg"
   description = "Private http inbound traffic"
   vpc_id      = var.vpc_id
 
   ingress {
-    description              = "HTTP from public SG"
-    from_port                = 8080
-    to_port                  = 8080
-    protocol                 = "tcp"
-    source_security_group_id = aws_security_group.public_http.id
+    description     = "HTTP from public SG"
+    from_port       = 8080
+    to_port         = 8080
+    protocol        = "tcp"
+    security_groups = [aws_security_group.public_http.id]
   }
 
   ingress {
-    description              = "ICMP from public SG"
-    from_port                = -1
-    to_port                  = -1
-    protocol                 = "icmp"
-    source_security_group_id = aws_security_group.public_http.id
+    description     = "ICMP from public SG"
+    from_port       = -1
+    to_port         = -1
+    protocol        = "icmp"
+    security_groups = [aws_security_group.public_http.id]
   }
 
   egress {
@@ -94,35 +94,41 @@ resource "aws_security_group" "private_http" {
   }
 
   tags = {
-    Name = "${var.project_id}-private-http-sg"
+    Name    = "${var.project_id}-private-http-sg"
     Project = var.project_id
   }
 }
 
-data "aws_instance" "public" {
-  instance_id = var.public_instance_id
+data "aws_network_interface" "public" {
+  filter {
+    name   = "attachment.instance-id"
+    values = [var.public_instance_id]
+  }
 }
 
-data "aws_instance" "private" {
-  instance_id = var.private_instance_id
+data "aws_network_interface" "private" {
+  filter {
+    name   = "attachment.instance-id"
+    values = [var.private_instance_id]
+  }
 }
 
 resource "aws_network_interface_sg_attachment" "public_ssh" {
   security_group_id    = aws_security_group.allow_ssh.id
-  network_interface_id = data.aws_instance.public.primary_network_interface_id
+  network_interface_id = data.aws_network_interface.public.id
 }
 
 resource "aws_network_interface_sg_attachment" "public_http" {
   security_group_id    = aws_security_group.public_http.id
-  network_interface_id = data.aws_instance.public.primary_network_interface_id
+  network_interface_id = data.aws_network_interface.private.id
 }
 
 resource "aws_network_interface_sg_attachment" "private_ssh" {
-  security_group_id    = aws_security_group.ssh.id
-  network_interface_id = data.aws_instance.private.primary_network_interface_id
+  security_group_id    = aws_security_group.allow_ssh.id
+  network_interface_id = data.aws_network_interface.private.id
 }
 
 resource "aws_network_interface_sg_attachment" "private_http" {
   security_group_id    = aws_security_group.private_http.id
-  network_interface_id = data.aws_instance.private.primary_network_interface_id
+  network_interface_id = data.aws_network_interface.private.id
 }
