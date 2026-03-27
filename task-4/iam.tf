68c0b7ddfbe4200819f485c5350ade30dd4556e8
kaipov24 <kaipovich24@gmail.com>
2026-03-27 20:42:35 +0600

    Task 4

---
+++ task-4/iam.tf
@@ -0,0 +1,61 @@
+resource "aws_iam_group" "epam-tf-practice" {
+  name = "cmtr-9upx8vyr-iam-group"
+  tags = {
+    Project = "cmtr-9upx8vyr"
+  }
+}
+
+resource "aws_iam_policy" "epam-tf-practice" {
+  name        = "cmtr-9upx8vyr-iam-policy"
+  path        = "/"
+  description = "My test policy"
+
+
+  policy = templatefile("${path.module}/policy.json", {
+    bucket_name = var.bucket
+  })
+
+  tags = {
+    Project = "cmtr-9upx8vyr"
+  }
+}
+
+resource "aws_iam_role" "epam-tf-practice" {
+  name = "cmtr-9upx8vyr-iam-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = {
+    Project = "cmtr-9upx8vyr"
+  }
+}
+
+resource "aws_iam_policy_attachment" "epam-tf-practice" {
+  name       = "cmtr-9upx8vyr-iam-role-attachment"
+  roles      = [aws_iam_role.epam-tf-practice.name]
+  policy_arn = aws_iam_policy.epam-tf-practice.arn
+
+  tags = {
+    Project = "cmtr-9upx8vyr"
+  }
+}
+
+resource "aws_iam_instance_profile" "epam-tf-practice" {
+  name = "cmtr-9upx8vyr-iam-instance-profile"
+  role = aws_iam_role.epam-tf-practice.name
+
+  tags = {
+    Project = "cmtr-9upx8vyr"
+  }
+}
---
+++ task-4/variables.tf
@@ -0,0 +1,9 @@
+variable "aws_region" {
+  description = "AWS region where resources will be created"
+  type        = string
+}
+
+variable "bucket" {
+  description = "S3 bucket name"
+  type        = string
+}
